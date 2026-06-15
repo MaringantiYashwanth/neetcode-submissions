860d63807e118eaafdd6e6e771ef4ce9d2b64157
MaringantiYashwanth <51596895+MaringantiYashwanth@users.noreply.github.com>
2026-06-15 07:46:04 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.js
@@ -0,0 +1,103 @@
+class Minheap {
+  constructor() {
+    this.heap = [];
+  }
+
+  push(val) {
+    this.heap.push(val);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  pop() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    const min = this.heap[0];
+    const end = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = end; // root will go to the end
+      this.sinkDown(0);
+    }
+    return min;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  bubbleUp(index) {
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[index] >= this.heap[parentIndex]) {
+        break;
+      }
+      [this.heap[index], this.heap[parentIndex]] = [
+        this.heap[parentIndex],
+        this.heap[index],
+      ];
+      index = parentIndex;
+    }
+  }
+
+  sinkDown(index) {
+    const length = this.heap.length;
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let smallest = index;
+      if (
+        leftChildIndex < length &&
+        this.heap[leftChildIndex] < this.heap[smallest]
+      ) {
+        smallest = leftChildIndex;
+      }
+      if (
+        rightChildIndex < length &&
+        this.heap[rightChildIndex] < this.heap[smallest]
+      ) {
+        smallest = rightChildIndex;
+      }
+      if (smallest === index) {
+        break;
+      }
+
+      [this.heap[index], this.heap[smallest]] = [
+        this.heap[smallest],
+        this.heap[index],
+      ];
+      index = smallest;
+    }
+  }
+}
+
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.k = k;
+        this.minHeap = new Minheap();
+        // Add all our initial values to stream tracking structure
+        for (let num of nums) {
+            this.add(num);
+        }
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.minHeap.push(val);
+        if (this.minHeap.size() > this.k) {
+            this.minHeap.pop();
+        }
+        //  The top of the minHeap is now the Kth largest element
+        return this.minHeap.peek();
+    }
+}
